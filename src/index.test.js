5fc6d44ec22684cfa63fb46d3ac5c1e321979f07
Kristofer Palmvik <kristofer@palmvik.se>
2015-10-26 17:31:44 +0100

    Add ES6 linting using eshint

--- src/index.test.js
+++ src/index.test.js
@@ -4,5 +4,5 @@ describe('calling the default Hello function', function(){
   it('should return Hello',function(){
     var hello = require('./index.js');
     expect(hello()).to.equal('Hello');
-    })
-  })
+  });
+});
