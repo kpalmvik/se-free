ae81bc7c0bf32acf55d209a96c58955a1ab66344
Kristofer Palmvik <kristofer@palmvik.se>
2015-10-26 15:46:12 +0100

    Add Babel and Travis support

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+exports['default'] = Hello;
+
+function Hello() {
+  return 'Hello';
+}
+
+module.exports = exports['default'];
