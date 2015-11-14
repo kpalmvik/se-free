dc91354796394d08f3991dabbb91c137df6b5830
Kristofer Palmvik <kristofer@palmvik.se>
2015-11-14 21:34:56 +0100

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,26 @@
 Object.defineProperty(exports, '__esModule', {
   value: true
 });
-exports['default'] = Hello;
+exports['default'] = isFree;
 
-function Hello() {
-  return 'Hello';
+function isFree(domain) {
+  var request = require('request-promise'),
+      domainAvailabilityUrl = 'http://free.iis.se/free?q=';
+
+  return request(domainAvailabilityUrl + domain).then(function (data) {
+    var returnValues = {
+      'free': 'FREE',
+      'not_valid': 'NOT_VALID',
+      'occupied': 'OCCUPIED'
+    },
+        domainAvailability = data.split(' ')[0];
+
+    if (returnValues[domainAvailability]) {
+      return returnValues[domainAvailability];
+    }
+
+    reject();
+  });
 }
 
 module.exports = exports['default'];
