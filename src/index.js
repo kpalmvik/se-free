ae0180718c4d78bf27bf2142fac4044583ac72b2
Kristofer Palmvik <kristofer@palmvik.se>
2019-09-26 17:19:04 +0200

    Use Eslint with airbnb-base

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
+import request from 'request-promise';
+import uts46 from 'idna-uts46';
+
 export default function isFree(domain) {
-  const request = require('request-promise'),
-        uts46 = require('idna-uts46'),
-        domainAvailabilityUrl = 'http://free.iis.se/free?q=',
-        idnEncodedDomain = uts46.toAscii(domain);
+  const domainAvailabilityUrl = 'http://free.iis.se/free?q=';
+  const idnEncodedDomain = uts46.toAscii(domain);
 
   return request(domainAvailabilityUrl + idnEncodedDomain)
     .then((data) => {
       const returnValues = {
-              'free': 'FREE',
-              'not_valid': 'NOT_VALID',
-              'occupied': 'OCCUPIED'
-            },
-            domainAvailability = data.split(' ')[0];
+        free: 'FREE',
+        not_valid: 'NOT_VALID',
+        occupied: 'OCCUPIED',
+      };
+      const domainAvailability = data.split(' ')[0];
 
-      if(returnValues[domainAvailability]) {
+      if (returnValues[domainAvailability]) {
         return returnValues[domainAvailability];
       }
 
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,65 +1,65 @@
 import nock from 'nock';
 
-import isFree from '../src/index.js';
+import isFree from '../src/index';
 
 const serverUrl = 'http://free.iis.se/';
 
 describe('checking isFree for a domain', () => {
-  it('should resolve to FREE if the domain is available',() => {
+  it('should resolve to FREE if the domain is available', () => {
     const availableDomain = 'available.se';
     nock(serverUrl)
       .get('/free')
-      .query({q: availableDomain})
+      .query({ q: availableDomain })
       .reply(200, `free ${availableDomain}`);
 
     return expect(isFree(availableDomain)).resolves.toBe('FREE');
   });
 
-  it('should resolve to NOT_VALID if the domain is not valid',() => {
+  it('should resolve to NOT_VALID if the domain is not valid', () => {
     const invalidDomain = 'not_valid.se';
     nock(serverUrl)
       .get('/free')
-      .query({q: invalidDomain})
+      .query({ q: invalidDomain })
       .reply(200, `not_valid ${invalidDomain}`);
 
     return expect(isFree(invalidDomain)).resolves.toBe('NOT_VALID');
   });
 
-  it('should resolve to OCCUPIED if the domain is occupied',() => {
+  it('should resolve to OCCUPIED if the domain is occupied', () => {
     const occupiedDomain = 'occupied.se';
     nock(serverUrl)
       .get('/free')
-      .query({q: occupiedDomain})
+      .query({ q: occupiedDomain })
       .reply(200, `occupied ${occupiedDomain}`);
 
     return expect(isFree(occupiedDomain)).resolves.toBe('OCCUPIED');
   });
 
-  it('should reject if the call fails with a HTTP error',() => {
+  it('should reject if the call fails with a HTTP error', () => {
     const exampleDomain = 'example.se';
     nock(serverUrl)
       .get('/free')
-      .query({q: exampleDomain})
+      .query({ q: exampleDomain })
       .reply(500, `occupied ${exampleDomain}`);
 
     return expect(isFree(exampleDomain)).rejects.toThrow();
   });
 
-  it('should reject if the request returns an unknown response',() => {
+  it('should reject if the request returns an unknown response', () => {
     const exampleDomain = 'example.se';
     nock(serverUrl)
       .get('/free')
-      .query({q: exampleDomain})
+      .query({ q: exampleDomain })
       .reply(200, 'unknown-response');
 
     return expect(isFree(exampleDomain)).rejects.toThrow();
   });
 
-  it('should reject if the request does not return any content',() => {
+  it('should reject if the request does not return any content', () => {
     const exampleDomain = 'example.se';
     nock(serverUrl)
       .get('/free')
-      .query({q: exampleDomain})
+      .query({ q: exampleDomain })
       .reply(200, null);
 
     return expect(isFree(exampleDomain)).rejects.toThrow();
@@ -67,13 +67,13 @@ describe('checking isFree for a domain', () => {
 });
 
 describe('checking isFree for an internationalized domain', () => {
-  it('should resolve to FREE if the domain is available',() => {
+  it('should resolve to FREE if the domain is available', () => {
     const availableDomain = 'räksmörgås.se';
     const availableDomainEncoded = 'xn--rksmrgs-5wao1o.se';
 
     nock(serverUrl)
       .get('/free')
-      .query({q: availableDomainEncoded})
+      .query({ q: availableDomainEncoded })
       .reply(200, `free ${availableDomainEncoded}`);
 
     return expect(isFree(availableDomain)).resolves.toBe('FREE');
