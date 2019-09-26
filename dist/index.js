85584a33bd03a8ca93fcd946f33003b1b84c98f3
Kristofer Palmvik <kristofer@palmvik.se>
2019-09-26 13:39:10 +0200

    Rewrite tests to use Jest and modern Babel version

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,16 @@
-'use strict';
+export default function isFree(domain) {
+  const request = require('request-promise'),
+        uts46 = require('idna-uts46'),
+        domainAvailabilityUrl = 'http://free.iis.se/free?q=',
+        idnEncodedDomain = uts46.toAscii(domain);
 
-Object.defineProperty(exports, '__esModule', {
-  value: true
-});
-exports['default'] = isFree;
-
-function isFree(domain) {
-  var request = require('request-promise'),
-      uts46 = require('idna-uts46'),
-      domainAvailabilityUrl = 'http://free.iis.se/free?q=',
-      idnEncodedDomain = uts46.toAscii(domain);
-
-  return request(domainAvailabilityUrl + idnEncodedDomain).then(function (data) {
-    var returnValues = {
+  return request(domainAvailabilityUrl + idnEncodedDomain).then(data => {
+    const returnValues = {
       'free': 'FREE',
       'not_valid': 'NOT_VALID',
       'occupied': 'OCCUPIED'
     },
-        domainAvailability = data.split(' ')[0];
+          domainAvailability = data.split(' ')[0];
 
     if (returnValues[domainAvailability]) {
       return returnValues[domainAvailability];
@@ -26,5 +19,3 @@ function isFree(domain) {
     reject();
   });
 }
-
-module.exports = exports['default'];
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,83 +1,81 @@
-const chai = require('chai'),
-      should = chai.should(),
-      chaiAsPromised = require('chai-as-promised'),
-      nock = require('nock'),
-      isFree = require('../src/index.js'),
-      serverUrl = 'http://free.iis.se/';
+import nock from 'nock';
 
-chai.use(chaiAsPromised);
+import isFree from '../src/index.js';
+
+const serverUrl = 'http://free.iis.se/';
 
 describe('checking isFree for a domain', () => {
-  it('should return FREE if the domain is available',() => {
-    const availableDomain = 'available.se',
-          nockServer = nock(serverUrl)
-            .get('/free')
-            .query({'q': availableDomain})
-            .reply(200, 'free ' + availableDomain);
+  it('should resolve to FREE if the domain is available',() => {
+    const availableDomain = 'available.se';
+    nock(serverUrl)
+      .get('/free')
+      .query({q: availableDomain})
+      .reply(200, `free ${availableDomain}`);
 
-    return isFree(availableDomain).should.eventually.equal('FREE');
+    return expect(isFree(availableDomain)).resolves.toBe('FREE');
   });
 
-  it('should return NOT_VALID if the domain is not valid',() => {
-    const invalidDomain = 'not_valid.se',
-          nockServer = nock(serverUrl)
-            .get('/free')
-            .query({'q': invalidDomain})
-            .reply(200, 'not_valid ' + invalidDomain);
+  it('should resolve to NOT_VALID if the domain is not valid',() => {
+    const invalidDomain = 'not_valid.se';
+    nock(serverUrl)
+      .get('/free')
+      .query({q: invalidDomain})
+      .reply(200, `not_valid ${invalidDomain}`);
 
-    return isFree(invalidDomain).should.eventually.equal('NOT_VALID');
+    return expect(isFree(invalidDomain)).resolves.toBe('NOT_VALID');
   });
 
-  it('should return OCCUPIED if the domain is occupied',() => {
-    const occupiedDomain = 'occupied.se',
-          nockServer = nock(serverUrl)
-            .get('/free')
-            .query({'q': occupiedDomain})
-            .reply(200, 'occupied ' + occupiedDomain);
+  it('should resolve to OCCUPIED if the domain is occupied',() => {
+    const occupiedDomain = 'occupied.se';
+    nock(serverUrl)
+      .get('/free')
+      .query({q: occupiedDomain})
+      .reply(200, `occupied ${occupiedDomain}`);
 
-    return isFree(occupiedDomain).should.eventually.equal('OCCUPIED');
+    return expect(isFree(occupiedDomain)).resolves.toBe('OCCUPIED');
   });
 
   it('should reject if the call fails with a HTTP error',() => {
-    const exampleDomain = 'example.se',
-          nockServer = nock(serverUrl)
-            .get('/free')
-            .query({'q': exampleDomain})
-            .reply(500, 'occupied ' + exampleDomain);
+    const exampleDomain = 'example.se';
+    nock(serverUrl)
+      .get('/free')
+      .query({q: exampleDomain})
+      .reply(500, `occupied ${exampleDomain}`);
 
-    return isFree(exampleDomain).should.eventually.be.rejected;
+    return expect(isFree(exampleDomain)).rejects.toThrow();
   });
 
-  it('should reject if the call returns something unknown',() => {
-    const exampleDomain = 'example.se',
-          nockServer = nock(serverUrl)
-            .get('/free')
-            .query({'q': exampleDomain})
-            .reply(200, 'unknown-response');
+  it('should reject if the request returns an unknown response',() => {
+    const exampleDomain = 'example.se';
+    nock(serverUrl)
+      .get('/free')
+      .query({q: exampleDomain})
+      .reply(200, 'unknown-response');
 
-    return isFree(exampleDomain).should.eventually.be.rejected;
+    return expect(isFree(exampleDomain)).rejects.toThrow();
   });
 
-  it('should reject if the call does not return any content',() => {
-    const exampleDomain = 'example.se',
-          nockServer = nock(serverUrl)
-            .get('/free')
-            .query({'q': exampleDomain})
-            .reply(200, null);
+  it('should reject if the request does not return any content',() => {
+    const exampleDomain = 'example.se';
+    nock(serverUrl)
+      .get('/free')
+      .query({q: exampleDomain})
+      .reply(200, null);
 
-    return isFree(exampleDomain).should.eventually.be.rejected;
+    return expect(isFree(exampleDomain)).rejects.toThrow();
   });
 });
 
 describe('checking isFree for an internationalized domain', () => {
-  it('should return FREE if the domain is available',() => {
-    const availableDomain = 'räksmörgås.se',
-          availableDomainEncoded = 'xn--rksmrgs-5wao1o.se',
-          nockServer = nock(serverUrl)
-            .get('/free')
-            .query({'q': availableDomainEncoded})
-            .reply(200, 'free ' + availableDomainEncoded)
+  it('should resolve to FREE if the domain is available',() => {
+    const availableDomain = 'räksmörgås.se';
+    const availableDomainEncoded = 'xn--rksmrgs-5wao1o.se';
 
-    return isFree(availableDomain).should.eventually.equal('FREE');
+    nock(serverUrl)
+      .get('/free')
+      .query({q: availableDomainEncoded})
+      .reply(200, `free ${availableDomainEncoded}`);
+
+    return expect(isFree(availableDomain)).resolves.toBe('FREE');
   });
 });
