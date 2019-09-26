3b36cfebd074b1c8c818c30e0c6cff0cd6fbd983
Kristofer Palmvik <kristofer@palmvik.se>
2019-09-26 17:53:19 +0200

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import request from 'request-promise';
 import uts46 from 'idna-uts46';
 
+const returnValues = {
+  free: 'FREE',
+  not_valid: 'NOT_VALID',
+  occupied: 'OCCUPIED',
+};
+
 export default function isFree(domain) {
-  const domainAvailabilityUrl = 'http://free.iis.se/free?q=';
   const idnEncodedDomain = uts46.toAscii(domain);
+  const requestUrl = `http://free.iis.se/free?q=${idnEncodedDomain}`;
 
-  return request(domainAvailabilityUrl + idnEncodedDomain)
+  return request(requestUrl)
     .then((data) => {
-      const returnValues = {
-        free: 'FREE',
-        not_valid: 'NOT_VALID',
-        occupied: 'OCCUPIED',
-      };
       const domainAvailability = data.split(' ')[0];
+      const result = returnValues[domainAvailability];
 
-      if (returnValues[domainAvailability]) {
-        return returnValues[domainAvailability];
+      if (result) {
+        return result;
       }
 
       return Promise.reject(new Error('Unknown error'));
