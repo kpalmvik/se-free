7d2b7f544e04d2eb67ef7d9f7bd71fb83d777964
Kristofer Palmvik <kristofer@palmvik.se>
2019-09-27 11:08:03 +0200

    1.0.1

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,34 @@
-export default function isFree(domain) {
-  const request = require('request-promise'),
-        uts46 = require('idna-uts46'),
-        domainAvailabilityUrl = 'http://free.iis.se/free?q=',
-        idnEncodedDomain = uts46.toAscii(domain);
+"use strict";
 
-  return request(domainAvailabilityUrl + idnEncodedDomain).then(data => {
-    const returnValues = {
-      'free': 'FREE',
-      'not_valid': 'NOT_VALID',
-      'occupied': 'OCCUPIED'
-    },
-          domainAvailability = data.split(' ')[0];
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = isFree;
 
-    if (returnValues[domainAvailability]) {
-      return returnValues[domainAvailability];
+var _requestPromise = _interopRequireDefault(require("request-promise"));
+
+var _idnaUts = _interopRequireDefault(require("idna-uts46"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var returnValues = {
+  free: 'FREE',
+  not_valid: 'NOT_VALID',
+  occupied: 'OCCUPIED'
+};
+
+function isFree(domain) {
+  var idnEncodedDomain = _idnaUts.default.toAscii(domain);
+
+  var requestUrl = `http://free.iis.se/free?q=${idnEncodedDomain}`;
+  return (0, _requestPromise.default)(requestUrl).then(function (data) {
+    var domainAvailability = data.split(' ')[0];
+    var result = returnValues[domainAvailability];
+
+    if (result) {
+      return result;
     }
 
-    reject();
+    return Promise.reject(new Error('Unknown error'));
   });
 }
