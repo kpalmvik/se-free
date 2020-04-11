a55c1d798701c897e5a50d1d5a09f83a6a0a925a
Kristofer Palmvik <kristofer@palmvik.se>
2020-04-11 08:50:37 +0200

    Build new version

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = isFree;
 
-var _requestPromise = _interopRequireDefault(require("request-promise"));
+var _nodeFetch = _interopRequireDefault(require("node-fetch"));
 
 var _idnaUts = _interopRequireDefault(require("idna-uts46"));
 
@@ -21,8 +21,14 @@ function isFree(domain) {
   var idnEncodedDomain = _idnaUts.default.toAscii(domain);
 
   var requestUrl = `http://free.iis.se/free?q=${idnEncodedDomain}`;
-  return (0, _requestPromise.default)(requestUrl).then(function (data) {
-    var domainAvailability = data.split(' ')[0];
+  return (0, _nodeFetch.default)(requestUrl).then(function (res) {
+    if (res.ok) {
+      return res.text();
+    }
+
+    return Promise.reject(new Error(res.statusText));
+  }).then(function (body) {
+    var domainAvailability = body.split(' ')[0];
     var result = returnValues[domainAvailability];
 
     if (result) {
