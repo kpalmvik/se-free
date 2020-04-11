62636dbb85f26e1ef37e274bf1f4fc1683719dfd
Kristofer Palmvik <kristofer@palmvik.se>
2020-04-11 07:51:42 +0200

    Refactor using node-fetch instead of request-promise

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import request from 'request-promise';
+import fetch from 'node-fetch';
 import uts46 from 'idna-uts46';
 
 const returnValues = {
@@ -11,9 +11,16 @@ export default function isFree(domain) {
   const idnEncodedDomain = uts46.toAscii(domain);
   const requestUrl = `http://free.iis.se/free?q=${idnEncodedDomain}`;
 
-  return request(requestUrl)
-    .then((data) => {
-      const domainAvailability = data.split(' ')[0];
+  return fetch(requestUrl)
+    .then((res) => {
+      if (res.ok) {
+        return res.text();
+      }
+
+      return Promise.reject(new Error(res.statusText));
+    })
+    .then((body) => {
+      const domainAvailability = body.split(' ')[0];
       const result = returnValues[domainAvailability];
 
       if (result) {
